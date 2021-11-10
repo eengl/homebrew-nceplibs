10cbabe7ba6d69fefb90b493d7880b20aa812949
Eric Engle <eengl@users.noreply.github.com>
2021-11-10 10:03:36 -0500

    Update nceplibs-g2.rb

--- Formula/nceplibs-g2.rb
+++ Formula/nceplibs-g2.rb
@@ -1,6 +1,6 @@
 class NceplibsG2 < Formula
-  desc "Utilities for coding/decoding GRIB2 messages."
-  homepage ""
+  desc "Fortran library for encoding/decoding GRIB2 messages."
+  homepage "https://github.com/NOAA-EMC/NCEPLIBS-g2"
   url "https://github.com/NOAA-EMC/NCEPLIBS-g2/archive/refs/tags/v3.4.5.tar.gz"
   sha256 "c18e991c56964953d778632e2d74da13c4e78da35e8d04cb742a2ca4f52737b6"
   license :public_domain
