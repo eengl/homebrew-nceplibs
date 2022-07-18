baec3feb97ccf2d632cc478d90acaa8c8bb9b316
Eric Engle <eengl@users.noreply.github.com>
2022-07-18 12:34:36 -0400

    Update nceplibs-bufr.rb
    v11.7

--- Formula/nceplibs-bufr.rb
+++ Formula/nceplibs-bufr.rb
@@ -1,8 +1,8 @@
 class NceplibsBufr < Formula
   desc "Fortran library, programs, and command line utilites for working with the BUFR format"
   homepage "https://github.com/NOAA-EMC/NCEPLIBS-bufr"
-  url "https://github.com/NOAA-EMC/NCEPLIBS-bufr/archive/refs/tags/bufr_v11.6.0.tar.gz"
-  sha256 "af4c04e0b394aa9b5f411ec5c8055888619c724768b3094727e8bb7d3ea34a54"
+  url "https://github.com/NOAA-EMC/NCEPLIBS-bufr/archive/refs/tags/bufr_v11.7.0.tar.gz"
+  sha256 "6a76ae8e7682bbc790321bf80c2f9417775c5b01a5c4f10763df92e01b20b9ca"
   license :public_domain
 
   depends_on "cmake" => :build
