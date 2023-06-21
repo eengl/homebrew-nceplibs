753942538e9b315c832a68c1cfcd9f2ad1f66c82
Eric Engle <eengl@users.noreply.github.com>
2023-06-21 10:43:37 -0400

    Update nceplibs-ip.rb
    Update version to v4.1
    
    build static first, then shared libs.

--- Formula/nceplibs-ip.rb
+++ Formula/nceplibs-ip.rb
@@ -1,8 +1,8 @@
 class NceplibsIp < Formula
   desc "NCEP General Interpolation Fortran Library"
   homepage "https://github.com/NOAA-EMC/NCEPLIBS-ip"
-  url "https://github.com/NOAA-EMC/NCEPLIBS-ip/archive/refs/tags/v4.0.0.tar.gz"
-  sha256 "a2ef0cc4e4012f9cb0389fab6097407f4c623eb49772d96eb80c44f804aa86b8"
+  url "https://github.com/NOAA-EMC/NCEPLIBS-ip/archive/refs/tags/v4.1.0.tar.gz"
+  sha256 "b83ca037d9a5ad3eb0fb1acfe665c38b51e01f6bd73ce9fb8bb2a14f5f63cdbe"
   license :public_domain
 
   depends_on "cmake" => :build
@@ -11,7 +11,10 @@ class NceplibsIp < Formula
   depends_on "nceplibs-sp"
 
   def install
-    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".", "-DCMAKE_PREFIX_PATH=#{prefix}"
+    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_PREFIX_PATH=#{prefix}", "-DOPENMP=ON", "."
+    system "make"
+    system "make", "install"
+    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_PREFIX_PATH=#{prefix}", "-DOPENMP=ON", "-DBUILD_SHARED_LIBS=ON", "."
     system "make"
     system "make", "install"
   end
