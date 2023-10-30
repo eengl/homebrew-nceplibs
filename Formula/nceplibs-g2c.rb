47e3d551fb4fa6211cad1820b7ff1258ef54c5f8
Eric Engle <eengl@users.noreply.github.com>
2023-10-30 12:05:11 -0400

    Update nceplibs-g2c.rb
    Updated g2c to v1.8.0

--- Formula/nceplibs-g2c.rb
+++ Formula/nceplibs-g2c.rb
@@ -1,22 +1,23 @@
 class NceplibsG2c < Formula
   desc "This library contains C decoder/encoder routines for GRIB edition 2."
   homepage "https://github.com/NOAA-EMC/NCEPLIBS-g2c"
-  url "https://github.com/NOAA-EMC/NCEPLIBS-g2c/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "73afba9da382fed73ed8692d77fa037bb313280879cd4012a5e5697dccf55175"
+  url "https://github.com/NOAA-EMC/NCEPLIBS-g2c/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "4ce9f5a7cb0950699fe08ebc5a463ab4d09ef550c050391a319308a2494f971f"
   license :public_domain
   
   depends_on "cmake" => :build
   depends_on "gcc" => :build
   depends_on "pkg-config" => :build
   depends_on "jasper"
+  depends_on "libaec"
   depends_on "libpng"
   depends_on "zlib"
 
   def install
-    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".", "-DUSE_PNG=ON", "-DUSE_Jasper=ON", "-DBUILD_SHARED_LIBS=OFF", "-DCMAKE_PREFIX_PATH=#{prefix}"
+    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".", "-DUSE_AEC=ON", "-DUSE_PNG=ON", "-DUSE_Jasper=ON", "-DBUILD_SHARED_LIBS=OFF", "-DCMAKE_PREFIX_PATH=#{prefix}"
     system "make"
     system "make", "install"
-    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".", "-DUSE_PNG=ON", "-DUSE_Jasper=ON", "-DBUILD_SHARED_LIBS=ON", "-DCMAKE_PREFIX_PATH=#{prefix}"
+    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".", "-DUSE_AEC=ON", "-DUSE_PNG=ON", "-DUSE_Jasper=ON", "-DBUILD_SHARED_LIBS=ON", "-DCMAKE_PREFIX_PATH=#{prefix}"
     system "make"
     system "make", "install"
   end
