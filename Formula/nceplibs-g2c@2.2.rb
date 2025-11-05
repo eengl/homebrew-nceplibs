5cdc9440e4bca881e0d5a6570c042567ee757941
Eric Engle <eengl@users.noreply.github.com>
2025-11-05 06:33:49 -0500

    Update NCEPLIBS-g2c to v2.3.0.

--- Formula/nceplibs-g2c@2.2.rb
+++ Formula/nceplibs-g2c@2.2.rb
@@ -1,8 +1,8 @@
 class NceplibsG2c < Formula
   desc "This library contains C decoder/encoder routines for GRIB edition 2."
   homepage "https://github.com/NOAA-EMC/NCEPLIBS-g2c"
-  url "https://github.com/NOAA-EMC/NCEPLIBS-g2c/archive/refs/tags/v2.3.0.tar.gz"
-  sha256 "8520a24c066500cfd0d07a05c6b7b0cb92383d1a4737cf6e79d9f4919c8e79ab"
+  url "https://github.com/NOAA-EMC/NCEPLIBS-g2c/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "cf0ac8f75aed662ccc64f4c44fbe46a70307bc27cbe95417fdfb6caf75245457"
   license :public_domain
   
   depends_on "cmake" => :build
