2f94f83000492474e5073a0152e7a8e3d5c5eaee
Eric Engle <eengl@users.noreply.github.com>
2026-09-23 12:34:17 -0400

    Update nceplibs-w3emc.

--- Formula/nceplibs-w3emc.rb
+++ Formula/nceplibs-w3emc.rb
@@ -1,5 +1,5 @@
 class NceplibsW3emc < Formula
-  desc "This library contains Fortran 90 decoder/encoder routines for GRIB Edition 1."
+  desc "Fortran 90 decoder/encoder routines for GRIB Edition 1"
   homepage "https://github.com/NOAA-EMC/NCEPLIBS-w3emc"
   url "https://github.com/NOAA-EMC/NCEPLIBS-w3emc/archive/refs/tags/v2.12.0.tar.gz"
   sha256 "77c0732541ade1deb381f5a208547ccc36e65efa91c8f7021b299b20a6ae0d27"
@@ -9,11 +9,34 @@ class NceplibsW3emc < Formula
   depends_on "gcc" => :build
   depends_on "pkg-config" => :build
   depends_on "nceplibs-bacio"
-  
+
   def install
-    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_PREFIX_PATH=#{prefix}", "."
-    system "make"
-    system "make", "install"
+    args = std_cmake_args + %W[
+      -DCMAKE_PREFIX_PATH=#{Formula["nceplibs-bacio"].opt_prefix}
+      -DBUILD_TESTING=OFF
+    ]
+
+    # Build static libraries
+    system "cmake", "-S", ".", "-B", "build_static", *args, "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "build_static"
+    system "cmake", "--install", "build_static"
+
+    # Build shared libraries
+    system "cmake", "-S", ".", "-B", "build_shared", *args, "-DBUILD_SHARED_LIBS=ON"
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+
+    # Symlink precision-specific include directories into prefix/include
+    # so Homebrew exposes them while keeping CMake export target paths happy
+    if (prefix/"include_4").exist?
+      include.install_symlink prefix/"include_4" => "include_4"
+    end
+    if (prefix/"include_8").exist?
+      include.install_symlink prefix/"include_8" => "include_8"
+    end
+    if (prefix/"include_d").exist?
+      include.install_symlink prefix/"include_d" => "include_d"
+    end
   end
 
   test do
